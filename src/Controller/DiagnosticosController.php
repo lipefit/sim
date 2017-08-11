b3bd9da29361b89bc774d038c8eadf3b46916e67
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-10 21:38:46 -0300

    Criado Controller Diagnosticos

---
+++ src/Controller/DiagnosticosController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Diagnósticos Controller
+ *
+ * @property \App\Model\Table\DiagnosticosTable $Diagnosticos
+ *
+ * @method \App\Model\Entity\Diagnosticos[] paginate($object = null, array $settings = [])
+ */
+class DiagnosticosController extends AppController {
+
+    public function initialize() {
+        parent::initialize();
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index() {
+        $idCliente = $this->Cookie->read('cliente_id');
+        
+        $diagnosticos = $this->Diagnosticos->find('all', [
+            'conditions' => [
+                'Identidades.cliente_id' => $idCliente
+            ]
+        ]);
+        $diagnostico = $diagnosticos->first();
+
+        if($diagnostico == null){
+            $diagnostico = $this->Diagnosticos->newEntity();
+        }
+        if ($this->request->is('post')) {
+            $this->request->data['Diagnosticos']['cliente_id'] = $this->Cookie->read('cliente_id');          
+            $diagnostico = $this->Diagnosticos->patchEntity($diagnostico, $this->request->getData());
+            if ($this->Diagnosticos->save($diagnostico)) {
+                $this->Flash->success(__('Dagnóstico atualizado com sucesso.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Diagnóstico não foi atualizado. Por favor, tente novamente'));
+        }
+        
+        $this->set(compact('diagnostico'));
+        $this->set('_serialize', ['diagnostico']);
+    }
+}
