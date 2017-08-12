f76c3cf1ada404a3dbb9048c3113e870c754fd9f
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-11 21:00:17 -0300

    Criado table mensagempauta

---
+++ src/Model/Table/MensagempautasTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class Mensagempautas extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->setTable('mensagem_pauta');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Pautas', [
+            'foreignKey' => 'pauta_id'
+        ]);
+
+        $this->belongsTo('Profiles', [
+            'foreignKey' => 'profile_id',
+        ]);
+
+    }
+}
