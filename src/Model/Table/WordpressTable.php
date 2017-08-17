6b500c40b671ca7e0da8f31be36a2bf63ca57510
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-17 19:58:15 -0300

    Criado tabela para wordpress

---
+++ src/Model/Table/WordpressTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class WordpressTable extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->setTable('wordpress');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Clientes', [
+            'foreignKey' => 'cliente_id'
+        ]);
+    }
+}
