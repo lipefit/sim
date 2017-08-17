5b124cd2fcc4d4b49d8b5a6eaea1af45f5bd7990
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-17 12:15:36 -0300

    Criado table conteudo

---
+++ src/Model/Table/ConteudosTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class ConteudosTable extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->setTable('conteudo');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Clientes', [
+            'foreignKey' => 'cliente_id'
+        ]);
+
+        $this->belongsTo('Pautas', [
+            'foreignKey' => 'pauta_id'
+        ]);
+
+    }
+    
+}
