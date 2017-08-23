e7bf1b2f06c4f41a9f1720b9624c9693e652c1de
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-23 20:02:17 -0300

    Criado table para o linkedin

---
+++ src/Model/Table/LinkedinsTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class LinkedinsTable extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->setTable('linkedin');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Clientes', [
+            'foreignKey' => 'cliente_id'
+        ]);
+
+    }
+    
+}
