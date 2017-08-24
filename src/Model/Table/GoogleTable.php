17f622c806bf3aa80257a2f3ea5727d9a30a590c
Felipe Almeida <lipe_fit@hotmail.com>
2017-08-24 20:35:51 -0300

    Criado table para google

---
+++ src/Model/Table/GoogleTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class GoogleTable extends Table {
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->setTable('google');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Clientes', [
+            'foreignKey' => 'cliente_id'
+        ]);
+
+    }
+    
+}
