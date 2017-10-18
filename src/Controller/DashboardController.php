41208a9114a0ac3362288fe0a0346485a2abd06d
Felipe Almeida <lipe_fit@hotmail.com>
2017-10-17 23:02:01 -0200

    Commit para pegar em outro notebook

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,24 @@ use Cake\Event\Event;
 class DashboardController extends AppController {
 
     public function index() {
-        
+        $this->loadModel("Conteudos");
+        $idCliente = $this->Cookie->read('cliente_id');
+
+        $porPersonas = $this->Conteudos->find('list', [
+            'select' => [
+                'Pautas.persona_id',
+                'Personapublicos.nome',
+                'count' => $this->Conteudos->find()->func()->count('Pautas.persona_id')
+            ],
+            'conditions' => [
+                'Conteudos.cliente_id' => $idCliente
+            ],
+            'group' => [
+                'Pautas.persona_id'
+            ],
+            'contain' => ['Pautas' => ['Personapublicos']]
+        ]);
+        $this->set(compact("porPersonas"));
     }
 
 }
