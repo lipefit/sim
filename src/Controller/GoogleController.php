fb6092a8fc288adee52fa71b515ae4d355f13365
Felipe Almeida <lipe_fit@hotmail.com>
2017-10-25 10:46:59 -0200

    Ajustada busca no analytics

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -89,72 +89,66 @@ class GoogleController extends AppController {
         if ($googles != null) {
             require_once(ROOT . DS . 'vendor' . DS . 'Google' . DS . 'google-api-php-client' . DS . 'src' . DS . 'Google' . DS . 'autoload.php');
             foreach ($googles as $google) {
-                $client = new \Google_Client();
-                $client->setAuthConfigFile('http://simarketing.provisorio.ws/webroot/client_secrets.json');
-                $client->addScope(\Google_Service_Analytics::ANALYTICS_READONLY);
-                $client->setAccessType("offline"); //habilita o modo Offline da API
-                $client->setApprovalPrompt("force");
+                try {
+                    $client = new \Google_Client();
+                    $client->setAuthConfigFile('http://simarketing.provisorio.ws/webroot/client_secrets.json');
+                    $client->addScope(\Google_Service_Analytics::ANALYTICS_READONLY);
+                    $client->setAccessType("offline"); //habilita o modo Offline da API
+                    $client->setApprovalPrompt("force");
 
-                if ($google['token'] != null) {
-                    $client->setAccessToken($google['token']);
-                    $analytics = new \Google_Service_Analytics($client);
-                    $date = date("Y-m-d H:i:s");
-                    $date = date('Y-m-d H:i:s', strtotime($date . '-1 day'));
-                    $explodeDate = explode(" ", $date);
-                    $data = $explodeDate[0];
+                    if ($google['token'] != null) {
+                        $client->setAccessToken($google['token']);
+                        $analytics = new \Google_Service_Analytics($client);
+                        $date = date("Y-m-d H:i:s");
+                        $date = date('Y-m-d H:i:s', strtotime($date . '-1 day'));
+                        $explodeDate = explode(" ", $date);
+                        $data = $explodeDate[0];
 
-                    // Pegar páginas
-                    $optParams = array(
-                        'dimensions' => 'ga:pageTitle,ga:pagePath',
-                        'sort' => '-ga:pageviews',
-                    );
-                    $results = $analytics->data_ga->get(
-                            'ga:' . $google['site'], '2012-01-01', $data, 'ga:pageviews', $optParams);
+                        // Pegar páginas
+                        $optParams = array(
+                            'dimensions' => 'ga:pageTitle,ga:pagePath',
+                            'sort' => '-ga:pageviews',
+                        );
+                        $results = $analytics->data_ga->get(
+                                'ga:' . $google['site'], '2012-01-01', $data, 'ga:pageviews', $optParams);
 
+                        $rows = $results->getRows();
 
-                    $resultsDaily = $analytics->data_ga->get(
-                            'ga:' . $google['site'], $data, date("Y-m-d"), 'ga:pageviews', $optParams);
+                        $this->getPages($google['cliente_id'], $rows);
 
-                    $rows = $results->getRows();
-                    $rowsDaily = $resultsDaily->getRows();
-
-                    $this->getPages($google['cliente_id'], $rows, $rowsDaily);
-
-                    // Pegar rejeição
-                    $optParamsRejeicao = array(
-                        'dimensions' => 'ga:pageTitle,ga:pagePath',
-                        'sort' => '-ga:bounceRate',
-                    );
-                    $resultsRejeicao = $analytics->data_ga->get(
-                            'ga:' . $google['site'], '2012-01-01', $data, 'ga:bounceRate', $optParamsRejeicao);
+                        // Pegar rejeição
+                        $optParamsRejeicao = array(
+                            'dimensions' => 'ga:pageTitle,ga:pagePath',
+                            'sort' => '-ga:bounceRate',
+                        );
+                        $resultsRejeicao = $analytics->data_ga->get(
+                                'ga:' . $google['site'], '2012-01-01', $data, 'ga:bounceRate', $optParamsRejeicao);
 
 
-                    $resultsDailyRejeicao = $analytics->data_ga->get(
-                            'ga:' . $google['site'], $data, date("Y-m-d"), 'ga:bounceRate', $optParamsRejeicao);
+                        $rowsRejeicao = $resultsRejeicao->getRows();
 
-                    $rowsRejeicao = $resultsRejeicao->getRows();
-                    $rowsDailyRejeicao = $resultsDailyRejeicao->getRows();
-
-                    $this->getRejeicao($google['cliente_id'], $rowsRejeicao, $rowsDailyRejeicao);
+                        $this->getRejeicao($google['cliente_id'], $rowsRejeicao);
+                    }
+                } catch (Exception $exc) {
+                    
                 }
             }
         }
         return $this->redirect(['action' => 'index']);
     }
 
-    function getPages($cliente, $rows, $rowsDaily) {
-        $this->loadModel("Analyticsdaily");
+    function getPages($cliente, $rows) {
         $this->loadModel("Analytics");
+        $this->loadModel("Cliente");
+        $this->loadModel("Revisaos");
 
-        $dailys = $this->Analyticsdaily->find('all', array(
-            'conditions' => array(
-                'Analyticsdaily.cliente_id' => $cliente
-            )
-        ));
-
-        foreach ($dailys as $d) {
-            $this->Analyticsdaily->delete($d);
-        }
+        $cls = $this->Cliente->find('all', [
+            'conditions' => [
+                'Cliente.id' => $cliente
+            ]
+        ]);
+        $cl = $cls->first();
+       
 
         $analytics = $this->Analytics->find('all', array(
             'conditions' => array(
@@ -167,28 +161,30 @@ class GoogleController extends AppController {
         }
 
         foreach (@$rows as $row) {
-            $newAnalytics = $this->Analytics->newEntity();
-            $this->request->data['Analytics']['pagina'] = $row[1];
-            $this->request->data['Analytics']['views'] = $row[2];
-            $this->request->data['Analytics']['cliente_id'] = $cliente;
-            $this->request->data['Analytics']['titulo'] = $row[0];
-            $newAnalytics = $this->Analytics->patchEntity($newAnalytics, $this->request->getData());
-            $this->Analytics->save($newAnalytics);
+            $revisaos = $this->Revisaos->find('all', array(
+                'conditions' => array(
+                    'Revisaos.url' => $cl->blog . $row[1]
+                ),
+            ));
+            
+            $contRevisao = $revisaos->count();
+            
+            if ($contRevisao > 0) {
+                $revisao = $revisaos->first();
+                $newAnalytics = $this->Analytics->newEntity();
+                $this->request->data['Analytics']['pagina'] = $row[1];
+                $this->request->data['Analytics']['views'] = $row[2];
+                $this->request->data['Analytics']['cliente_id'] = $cliente;
+                $this->request->data['Analytics']['titulo'] = $row[0];
+                $this->request->data['Analytics']['conteudo_id'] = $revisao->conteudo_id;
+                $newAnalytics = $this->Analytics->patchEntity($newAnalytics, $this->request->getData());
+                $this->Analytics->save($newAnalytics);
+            }
         }
 
-        foreach (@$rowsDaily as $rowDaily) {
-            $newDaily = $this->Analyticsdaily->newEntity();
-            $this->request->data['Analyticsdaily']['pagina'] = $row[1];
-            $this->request->data['Analyticsdaily']['views'] = $row[2];
-            $this->request->data['Analyticsdaily']['cliente_id'] = $cliente;
-            $this->request->data['Analyticsdaily']['titulo'] = $row[0];
-            $newDaily = $this->Analyticsdaily->patchEntity($newDaily, $this->request->getData());
-            $this->Analyticsdaily->save($newDaily);
-        }
     }
 
-    function getRejeicao($cliente, $rows, $rowsDaily) {
-        $this->loadModel("Analyticsdaily");
+    function getRejeicao($cliente, $rows) {
         $this->loadModel("Analytics");
 
         foreach (@$rows as $row) {
@@ -205,23 +201,6 @@ class GoogleController extends AppController {
                 $this->request->data['Analytics']['rejeicao'] = $row[2];
                 $a = $this->Analytics->patchEntity($a, $this->request->getData());
                 $this->Analytics->save($a);
-            }
-        }
-
-        foreach (@$rowsDaily as $rowDaily) {
-            $dailys = $this->Analyticsdaily->find('all', array(
-                'conditions' => array(
-                    'Analyticsdaily.cliente_id' => $cliente,
-                    'Analyticsdaily.pagina' => $rowDaily[1]
-                )
-            ));
-
-            $d = $dailys->first();
-
-            if ($d != null) {
-                $this->request->data['Analyticsdaily']['rejeicao'] = $rowDaily[2];
-                $d = $this->Analyticsdaily->patchEntity($d, $this->request->getData());
-                $this->Analyticsdaily->save($d);
             }
         }
     }
